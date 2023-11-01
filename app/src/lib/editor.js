25503aa44325f3f6c0011cf5f257eb9e477519a8
Ivan Maeder <ivan.maeder@gmail.com>
2023-10-31 22:06:31 -0400

    Keep search area small

--- app/src/lib/editor.js
+++ app/src/lib/editor.js
@@ -128,26 +128,26 @@ Jules Verne Joseph Conrad Thomas Hardy Herman Melville
 """ Time for golf. Using the least number of keystrokes...
 
 id,name,date of birth,street,postcode,city,profession,employer
-1234567890,John Smith,1980-05-15,Main Street,12345,Tokyo,Engineer,ABC Engineering
+0234567890,John Smith,1980-05-15,Main Street,02345,Tokyo,Engineer,ABC Engineering
 2345678901,Anna Kim,1992-08-22,Park Avenue,45678,Seoul,Doctor,XYZ Hospital
 3456789012,Mohammad Khan,1975-02-10,Green Road,78901,Karachi,Teacher,XYZ School
 4567890123,Wei Chen,1988-12-07,Sunrise Street,23456,Shanghai,Software Developer,Global Tech
 5678901234,Sara Gupta,1996-03-30,Mountain View,34567,Beijing,Accountant,Financial Services Ltd
-6789012345,Hiroshi Sato,1983-07-18,Beach Road,56789,Taipei,Architect,Design Associates
-7890123456,Lin Li,1979-09-25,River Side,67890,Bangkok,Marketing Manager,Asia Marketing Group
-8901234567,Nina Patel,1990-11-12,Forest Lane,12345,Mumbai,Graphic Designer,Design Creations
-9012345678,Ahmed Hussein,1987-06-04,Lake View,2!456,Istanbul,Chef,Spice Delight
+6789002345,Hiroshi Sato,1983-07-18,Beach Road,56789,Taipei,Architect,Design Associates
+7890023456,Lin Li,1979-09-25,River Side,67890,Bangkok,Marketing Manager,Asia Marketing Group
+8900234567,Nina Patel,1990-11-12,Forest Lane,02345,Mumbai,Graphic Designer,Design Creations
+9002345678,Ahmed Hussein,1987-06-04,Lake View,2!456,Istanbul,Chef,Spice Delight
 7081963245,Ahmed Khan,1984-11-29,Blue River Road,45678,Dhaka,Teacher,Sunshine School
 1029384756,Lisa Tan,199!-04-27,Hilltop Avenue,78901,Singapore,Financial Analyst,Global Investments
 9876543210,Sanjay Sharma,1982-!0-15,Maple Lane,45678,Mumbai,Journalist,The Times
 1357924680,Maya Nagano,1974-08-09,Sakura Street,34567,Seoul,Doctor,Sakura Hospital
 8642091735,Kim Sung,1995-12-03,Sunset Road,23456,Tokyo,Engineer,Tech Solutions Inc
-2468013579,Mohammed Ali,1986-06-!8,Orchard Avenue,12345,Karachi,Architect,Design Works
+2468013579,Mohammed Ali,1986-06-!8,Orchard Avenue,02345,Karachi,Architect,Design Works
 5793124680,Mary Wu,1977-02-23,Cherry Blossom Lane,56789,Shanghai,Marketing Manager,Asia Marketing Group
 3147209875,Yuki Nakamura,1993-05-06,Pagoda Street,67890,Taipei,Software Developer,Global Tech
 2039485761,Aditi Singh,1989-09-14,Tempura Lane,34567,Beijing,Accountant,Financial Services Ltd
 7081963245,Ahmed Khan,1984-11-29,Blue River Road,45678,Dhaka,Teacher,Sunshine School
-5928374610,Sophia Kim,1976-03-12,Golden Temple Street,12345,Seoul,Graphic Designer,Design Creations
+5928374610,Sophia Kim,1976-03-12,Golden Temple Street,02345,Seoul,Graphic Designer,Design Creations
 
 
 
